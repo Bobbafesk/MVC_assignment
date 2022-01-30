7ef7b07d1bdb9544bd2db1ac4533a740e90178ad
Bobbafesk <a.torin@gmail.com>
2022-01-30 21:26:43 +0100

    This is the first assignment in MVC

--- MVC_assignment/Controllers/HomeController.cs
+++ MVC_assignment/Controllers/HomeController.cs
@@ -10,20 +10,43 @@ namespace MVC_assignment.Controllers
     {
         public IActionResult Index()
         {
+            
             return View();
         }
 
         public IActionResult About()
         {
+            ViewBag.year1 = "2021 - 2022 Course at Lexicon";
+            ViewBag.content1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+            ViewBag.year2 = "2020 - 2021 Made some money";
+            ViewBag.content2 = "Mauris augue massa, suscipit non interdum et, facilisis quis augue.";
+            ViewBag.year3 = "2021 - 2022 Lorem";
+            ViewBag.content3 = "Vestibulum eget turpis ut leo viverra eleifend. Fusce nec ipsum et nibh consectetur lacinia.";
+
             return View();
         }
 
         public IActionResult Contact()
         {
+            ViewBag.adress = "Bond street 1, London, England.";
+            ViewBag.mobile = "+44 70 255255";
+            ViewBag.email = "elev@lexicon.com";
             return View();
         }
         public IActionResult Projects()
         {
+            ViewBag.project1 = "https://github.com/Bobbafesk/ConsoleCalculatorBetter";
+            ViewBag.description1 = "I nice simple console calculator.";
+            ViewBag.project2 = "https://github.com/Bobbafesk/ConsoleHangman";
+            ViewBag.description2 = "The game Hang man in console.";
+            ViewBag.project3 = "https://github.com/Bobbafesk/VendingMachineController";
+            ViewBag.description3 = "A vending machine stub to use.";
+            ViewBag.project4 = "https://github.com/Bobbafesk/CSS-uppgift";
+            ViewBag.description4 = "A html and css assignment.";
+            ViewBag.project5 = "https://github.com/Bobbafesk/Sokoban-assignment";
+            ViewBag.description5 = "The game Sokoban.";
+            ViewBag.project6 = "https://github.com/Bobbafesk/MVC_assignment";
+            ViewBag.description6 = "MVC project.";
             return View();
         }
     }
--- MVC_assignment/Startup.cs
+++ MVC_assignment/Startup.cs
@@ -16,7 +16,8 @@ namespace MVC_assignment
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            //services.AddMvc();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +28,7 @@ namespace MVC_assignment
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
